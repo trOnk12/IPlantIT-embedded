0cb4cbeb91543d2746753427e36ee727f6178ddf
Mateusz Pachulski <mateusz.pachulski@appunite.com>
2021-05-27 18:11:54 +0200

    emit service to define device name

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,26 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 
-#define SERVICE_UUID        "84b3571a-98d0-46fe-95c2-bb2fc4b126ad"
-#define CHARACTERISTIC_UUID "547de50b-14b8-49fc-9c88-deb5b65cf716"
+#define ADVERTISMENT_SERVICE_UUID      "84b3571a-98d0-46fe-95c2-bb2fc4b126ad"
+
+#define GENERAL_INFO_SERVICE_UUID "3057b961-e173-4aae-8432-565f123daaa5"
+#define DEVICE_NAME_CHARACTERISTIC_UUID "547de50b-14b8-49fc-9c88-deb5b65cf716"
 
 
-class WiFiSSIDCallback : public BLECharacteristicCallbacks{
+class DeviceNameCallback : public BLECharacteristicCallbacks{
     void onWrite(BLECharacteristic* pCharactersitic){
-    
-        Serial.println("Characteristic defined! Now you can read it in your phone!");
+        Serial.println("Device name set");
     }
+};
+
+class ConnectionCallback : public BLEServerCallbacks{
+  void onConnect(BLEServer* pServer){
+      Serial.println("Device connceted");
+  }
+
+  void onDisconnect(BLEServer* pServer){
+        Serial.println("Device disconnected");
+  }
 };
 
 void setup() {
@@ -20,27 +31,29 @@ void setup() {
 
   BLEDevice::init("I-Plant");
   BLEServer *pServer = BLEDevice::createServer();
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-                                         CHARACTERISTIC_UUID,
-                                         BLECharacteristic::PROPERTY_READ |
-                                         BLECharacteristic::PROPERTY_WRITE
-                                       );
 
-  pCharacteristic->setValue("Hello World says Neil");
-  pCharacteristic->setCallbacks(new WiFiSSIDCallback());
-  pService->start();
-  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->addServiceUUID(ADVERTISMENT_SERVICE_UUID);
   pAdvertising->setScanResponse(true);
   pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
   pAdvertising->setMinPreferred(0x12);
+
+  pServer->setCallbacks(new ConnectionCallback());
+
+      BLEService *pService = pServer->createService(GENERAL_INFO_SERVICE_UUID);
+      BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         DEVICE_NAME_CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+      pCharacteristic->setValue("Hello World says Neil");
+      pCharacteristic->setCallbacks(new DeviceNameCallback());
+      pService->start();
+
   BLEDevice::startAdvertising();
-  Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
     delay(2000);
+       
 }
